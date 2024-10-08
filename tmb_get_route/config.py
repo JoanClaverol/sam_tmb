495fd7e1cb0d24a61820e6459d03b5e6f12e1cf3
Joan Claverol <41630699+JoanClaverol@users.noreply.github.com>
2024-10-08 19:03:01 +0200

    Update config.py

--- tmb_get_route/config.py
+++ tmb_get_route/config.py
@@ -2,14 +2,14 @@ BASE_URL = "https://api.tmb.cat/v1/"
 ENDPOINT = "planner/plan"
 BUCKET = "tmbinfo"
 BUCKET_FOLDER = "routes_from_api"
-TMB_APP_ID = "8de5b882"
-TMB_APP_KEY = "a9e0b1a36bd4417ffd5a815b186d043d"
+TMB_APP_ID = "" # your id key here
+TMB_APP_KEY = "" # your api key here
 HOME_LOCATION = {
-    'latitude': 41.423043,
-    'longitude': 2.184006
+    'latitude': 42.423043,
+    'longitude': 2.204006
 }
 
 WORK_LOCATION = {
-    'latitude': 41.406232,
-    'longitude': 2.192273
+    'latitude': 42.416232,
+    'longitude': 2.212273
 }
